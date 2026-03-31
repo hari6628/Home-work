5a98a29fbefbf6c23fdfae0f010ebb676ba900a3
hari6628 <you@example.com>
2026-03-31 15:46:07 +0530

    Homework

---
+++ Evening.java
@@ -0,0 +1,7 @@
+class Evening {
+   public static void main (String[]args){
+         System.out.println("Good Evening");  
+   
+   }
+
+}
---
+++ Success.java
@@ -0,0 +1,7 @@
+class Success{
+   public static void main (String[]args){
+         System.out.println("Success is yours");  
+   
+   }
+
+}
