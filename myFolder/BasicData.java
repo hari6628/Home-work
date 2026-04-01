e87df2de03a405474381b2461d01defed2266bf6
hari6628 <you@example.com>
2026-04-01 19:39:30 +0530

    Homework

---
+++ myFolder/BasicData.java
@@ -0,0 +1,21 @@
+class BasicData
+
+{
+	
+	
+	public static void main (String[] args)
+	{
+		String name="Hari";
+	  int age = 22;
+	char grade='B';
+	
+	boolean passed =true ;
+	
+	System.out.println("Name: "+name);
+	System.out.println("Age: "+age);
+	System.out.println("Grade: "+grade);
+	System.out.println("Passed: "+passed);
+		
+		
+	}
+}
---
+++ myFolder/Marks.java
@@ -0,0 +1,23 @@
+class Marks {
+    public static void main(String[] args) 
+{
+	
+	int mark1 = 80;
+    int mark2 = 90;
+    int mark3 = 70;
+	
+	int total = mark1+mark2+mark3;
+	int average = mark1+mark2+mark3/3;
+	
+	System.out.println("Total = "+total);
+		System.out.println("Average = "+average);
+
+	
+
+	
+	
+	
+	
+	
+	
+}}
