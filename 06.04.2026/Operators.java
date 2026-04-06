6695a1ce662dfdfe88b41066ddfbaf48b7514ce9
hari6628 <you@example.com>
2026-04-06 15:33:19 +0530

    Homework

---
+++ 06.04.2026/Operators.java
@@ -0,0 +1,19 @@
+class Operators
+{
+	public static void main (String [] args)
+	{
+		
+		int a = 40;
+		int b = 20;
+		//Arithmetic Operation
+		System.out.println("Addition = " + (a+b));
+		System.out.println("Subraction = "+(a-b));
+		System.out.println("Division = "+(a/b));
+		System.out.println("Modulus = "+(a%b));
+		System.out.println("Multiplication = " +(a*b));
+		
+		
+	}
+	
+	
+}
