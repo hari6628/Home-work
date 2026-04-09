c6746b7177bbf149805d955c34c375301910b79a
hari6628 <you@example.com>
2026-04-09 22:12:27 +0530

    Homework

--- 09.04.2026/Days.java
+++ 09.04.2026/Days.java
@@ -1,57 +1,76 @@
 import java.util.Scanner;
-
 class Days
+
 {
 public static void main (String [] args)
-{
 
-Scanner sc = new Scanner(System.in);
-
-
-System.out.print("What day is  today? : ");
-
-int day = sc.nextInt();
-if (day == 1){
-	System.out.print("Today is : ");
-	System.out.println("Monday");
 	
-}
-
-else if (day == 2){
-	System.out.print("Today is : ");
-	System.out.println("Tuesday");
-}
-
-else if (day == 3){
-	System.out.print("Today is : ");
-	System.out.println("Wednesday");
-}
-
-else if (day == 4){
-	System.out.print("Today is : ");
-	System.out.println("Thursday");
-}
-
-else if (day == 5){
-	System.out.print("Today is : ");
-	System.out.println("Friday");
-}
-
-else if (day == 6){
-	System.out.print("Today is : ");
-	System.out.println("Saturday");
-}
-
-else if (day == 7){
-	System.out.print("Today is : ");
-	System.out.println("Sunday");
-}
-
-else {System.out.println("Invalid");}
-
-
-
-}
-
-
+	{
+		Scanner sc = new  Scanner(System.in);
+		System.out.print("Enter the number : ");
+		int day  = sc.nextInt();
+		switch(day)
+		{
+			
+			case 1:
+			{
+				
+				System.out.println("Monday");
+				break;
+				
+			}
+			
+			case 2:
+			{
+				
+				System.out.println("Tuesday");
+				break;
+			}
+			
+			case 3:
+			{
+				
+				System.out.println("Wednesday");
+				break;
+			}
+			case 4:
+			{
+				
+				System.out.println("Thursday");
+				break;
+			}
+			case 5:
+			{
+				
+				System.out.println("Friday");
+				break;
+			}
+			
+			case 6:
+			{
+				
+				System.out.println("Saturday");
+				break;
+			}
+			
+			case 7:
+			{
+				
+				System.out.println("Sunday");
+				break;
+			}
+			
+			default:
+                System.out.println("Invalid");
+			break;
+			
+			
+		}
+		
+		
+		
+		System.out.println();
+		
+	}
+	
 }
