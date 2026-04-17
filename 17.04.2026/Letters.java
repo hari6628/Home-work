feda62be3bec9f78f9292a9bc6768ab6776833fa
hari6628 <you@example.com>
2026-04-17 16:06:36 +0530

    Homework

---
+++ 17.04.2026/Letters.java
@@ -0,0 +1,22 @@
+
+class Letters
+{
+	public static void main (String [] args)
+	{
+		char a = 'A';
+		
+		for(int i = 1; i <=5; i++ )
+		{
+			
+				
+			for(int j = 1; j<=i; j++)
+			{
+				System.out.print(a+" ");
+				a++;
+			}
+			
+			 System.out.println(" ");
+		}
+	}
+}
+		
